34257e5d34829df9ef39c50ab2b8b1ff0b78eaa8
Matheus Carvalho <matheusvca@gmail.com>
2020-10-27 04:16:06 -0300

    4/9 challenges solved

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -11,11 +12,23 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push(repo);
+
+  return response.json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
@@ -27,6 +40,17 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+  
+  const foundRepo = repositories.find(repo=>repo.id === id);
+
+  if(!foundRepo){
+    return response.status(400).send();
+  }
+
+  foundRepo.likes += 1;
+
+  return response.json({likes: foundRepo.likes});
   // TODO
 });
 
